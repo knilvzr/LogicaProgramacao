f1137f766d5908ff5674b74c5b90b99de5d1f9c4
knilvzr <kraus2469@gmail.com>
2025-10-08 23:08:39 -0300

    .

--- Modulo2Java.Script/desafio/cadastro.js
+++ Modulo2Java.Script/desafio/cadastro.js
@@ -1,26 +1,40 @@
-let p = document.getElementById("p")
 
+let p = document.getElementById("p");
 let alunos = [
-    {nome:"evelyn", nota: 0}
-]
+    {nome: "evelyn", nota: 0}
+];
 
+
+function exibirAlunos() {
+    p.innerHTML = "";
+    for (let aluno of alunos) {
+        p.innerHTML += `${aluno.nome} possui nota ${aluno.nota}<br>`;
+    }
+}
+
+
+exibirAlunos();
 
 function adicionarAluno() {
-    let nome = document.getElementById("nome").value 
-    let nota = document.getElementById("nota").value 
-    alunos.unshift ({nome, nota})
-    atualizarLista()
+    let nome = document.getElementById("nome").value;
+    let nota = document.getElementById("nota").value;
+    
+
+    if (nome === "" || nota === "" || nota > 10 ) {
+        alert("Por favor, preencha nome e nota!");
+        return;
+    }
+    
+    alunos.unshift({nome, nota: Number(nota)});
+
+    document.getElementById("nome").value = "";
+    document.getElementById("nota").value = "";
+    
+    exibirAlunos();
 }
 
-let aluno = ({nome, nota})
 
-function atualizarLista() {
 
-alunos.forEach((aluno) => {
-p.innerHTML += (` ${aluno.nome} possui nota ${aluno.nota}<br>`);
-})
-}
-atualizarLista()
 
 
 
